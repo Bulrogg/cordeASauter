76346c78df45497b64b96c6d12c394a7449e4450
Millard Frédéric <fmi@octo.com>
2015-03-10 15:01:07 +0100

    Init projet yo angular + ngRoute

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cordeAsauterApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cordeAsauterApp
+ */
+angular.module('cordeAsauterApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cordeAsauterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cordeAsauterApp
+ */
+angular.module('cordeAsauterApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
