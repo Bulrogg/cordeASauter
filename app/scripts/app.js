a5fb572c530e63f949e643c94583b40e92b9f7da
Millard Frédéric <fmi@octo.com>
2015-08-17 09:36:44 +0200

    suppression ng-route + mise en place du tableau

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,28 +1,3 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name cordeAsauterApp
- * @description
- * # cordeAsauterApp
- *
- * Main module of the application.
- */
-angular
-  .module('cordeAsauterApp', [
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('corde-a-sauter-app', [ ] );
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,24 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name cordeAsauterApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the cordeAsauterApp
- */
-angular.module('cordeAsauterApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('corde-a-sauter-app').controller('main-controller', ['$scope', function ($scope) {
+
+  $scope.configurationEntrainements = [
+    { dureeTotal:5*60, tempsEffort:10, tempsRecuperation:20 },
+    { dureeTotal:8*60, tempsEffort:10, tempsRecuperation:20 },
+    { dureeTotal:10*60, tempsEffort:15, tempsRecuperation:15 },
+    { dureeTotal:12*60, tempsEffort:15, tempsRecuperation:15 },
+    { dureeTotal:15*60, tempsEffort:20, tempsRecuperation:10 },
+    { dureeTotal:18*60, tempsEffort:20, tempsRecuperation:10 },
+    { dureeTotal:20*60, tempsEffort:25, tempsRecuperation:10 },
+    { dureeTotal:20*60, tempsEffort:30, tempsRecuperation:10 },
+    { dureeTotal:20*60, tempsEffort:40, tempsRecuperation:10 },
+    { dureeTotal:25*60, tempsEffort:50, tempsRecuperation:10 },
+    { dureeTotal:25*60, tempsEffort:60, tempsRecuperation:10 },
+    { dureeTotal:30*60, tempsEffort:60, tempsRecuperation:10 },
+    { dureeTotal:30*60, tempsEffort:75, tempsRecuperation:10 },
+    { dureeTotal:30*60, tempsEffort:90, tempsRecuperation:10 },
+    { dureeTotal:30*60, tempsEffort:105, tempsRecuperation:10 },
+    { dureeTotal:30*60, tempsEffort:120, tempsRecuperation:10 }
+  ];
+
+}]);
